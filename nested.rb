7700d173afecbd40dfc27e2297bded899c9465d2
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 17:35:29 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +56,17 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages].join(",")
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  
+  matz_hash = {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      }
+  }
 
 	programmer_hash = 
  		{
@@ -80,6 +84,9 @@ def adding_matz
         }
      }
 
+programmer_hash = matz_hash.merge!(programmer_hash)
+
+return programmer_hash
     
 end
 
@@ -100,8 +107,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      
 end
 
@@ -122,4 +129,6 @@ def adding_to_dennis
         }
      }
 
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+  return programmer_hash
 end
